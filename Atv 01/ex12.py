adf584cf7dda096ebe1525027ee00a1da96c5f12
srchby <raidentsuji@gmail.com>
2025-03-25 19:08:54 -0300

    corrected exercises

--- Atv 01/ex12.py
+++ Atv 01/ex12.py
@@ -1,15 +1,27 @@
 def classAvg(classes, students):
-    avg = students // classes
-    return avg
+    avg = students / classes
+    stuByCl = []
+    distStu = 0
+    for i in range(classes):
+        remainingStu = students - distStu
+        remainingCl = classes - i
+        stuInCl = round(avg * (i + 1)) - round(avg * i)
+        stuByCl.append(stuInCl)
+        distStu += stuInCl
+        
+    return stuByCl
 
 while True:
-    schClass = input("Envie a quantidade de salas: ")
-    schStudents = input("Envie a quantidade total de alunos: ")
+    schClass = int(input("Envie a quantidade de salas: "))
+    schStudents = int(input("Envie a quantidade total de alunos: "))
     schAvg = classAvg(schClass, schStudents)
     
-    if (schAvg > 40):
-        print("A média é mais de 40 alunos por sala!")
-        break
-    else:
-        print(f"A média de alunos por classe é {schAvg}")
-        break
+    print(f"Alunos para {schClass} turmas: ")
+    biggerThanForty = False;
+    for i, stu in enumerate(schAvg, start=1):
+        print(f"Turma {i}: {stu} Estudantes")
+        if (stu > 40):
+            biggerThanForty = True
+            
+    if (biggerThanForty):
+        print("Há alguma sala com mais de 40 alunos")
