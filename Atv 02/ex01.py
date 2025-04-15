2fb3872f056bc7ddaf1422f84657f55d53d36d17
srchby <raidentsuji@gmail.com>
2025-04-15 14:58:26 -0300

    ex01.py complete

--- Atv 02/ex01.py
+++ Atv 02/ex01.py
@@ -1,20 +1,59 @@
-tasks = [["clean", "13 april", False],["exercise","14 april", True]]
+import json
+
+tasks = []
+
+def save_tasks():
+    with open("tasks.json", "w") as outfile:
+        json.dump(tasks, outfile, indent=4)
+        
+def load_tasks():
+    global tasks
+    try:
+        with open("tasks.json", "r") as infile:
+            tasks = json.load(infile)
+    except FileNotFoundError:
+        tasks = []
+    
+def get_task_date(task):
+    day, month, year = map(int, task[1].split('/'))
+    return (year, month, day)
+
+def sorted_tasks(task_list):
+    return sorted(task_list, key=get_task_date)
+
+def add_task():
+    name = input("Nome da tarefa: \n>> ")
+    date = input("Prazo da tarefa (dd/mm/yy): \n>> ")
+    day, month, year = map(int, date.split('/'))
+    tasks.append([name, date, False])   
+
+def mark_task():
+    index = input(f"Escolher tarefa \n>> ")
+    try:
+        index = int(index)
+        if 0 <= index < len(tasks):
+            tasks[index][2] = True
+        else:
+            print("\nTarefa inválida\n")
+    except ValueError:
+        print(f"\nValor não válido\n")
+
+load_tasks()
+
 while True:
+    tasks = sorted_tasks(tasks)
     print(f"Tarefas: ")
     for i, task in enumerate(tasks):
-        print(f"{i}. \nNome: {task[0]} \nPrazo: {task[1]} \nCompleto: {task[2]}")
-    action = int(input(f"1. Adicionar tarefa \n2. Completar tarefa \n>> "))
+        print(f"\n{i}. \nNome: {task[0]} \nPrazo: {task[1]} \nCompleto: {task[2]}\n")
+    
+    save_tasks()
+    
+    action = int(input(f"1. Adicionar tarefa \n2. Completar tarefa \n3. Sair \n>> "))
     if (action == 1):
-        name = input("Nome da tarefa: \n>> ")
-        date = input("Prazo da tarefa: \n>> ")
-        tasks.append([name, date, False])   
+        add_task()
     elif (action == 2):
-        index = input(f"Escolher tarefa \n>> ")
-        try:
-            index = int(index)
-            if 0 <= index < len(tasks):
-                tasks[index][2] = True
-            else:
-                print("Tarefa inválida")
-        except ValueError:
-            print(f"Valor não válido")
+        mark_task()
+    elif (action == 3):
+        save_tasks()
+        break
+        
